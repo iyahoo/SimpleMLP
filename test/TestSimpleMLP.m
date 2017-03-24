e23d4f7412c8e491d2145256d34699f7605c5de1
iyahoo <s1200191@gmail.com>
2017-03-24 11:53:52 +0900

    [Add] setupOnce [Fix] all tests

--- test/TestSimpleMLP.m
+++ test/TestSimpleMLP.m
@@ -2,13 +2,14 @@ function tests = TestSimpleMLP
     tests = functiontests( localfunctions );
 end
 
-function testUnion( testCase )
-% Requier Statistic and Machine Learning Toolbox
-    addpath( '../' );
+function setupOnce( testCase )
+    fprintf( '\nNow setuping test ...\n' );
 
-    % First column is class label, others are features.
+    addpath( '../' ); % Add @SimpleMLP
+
     dataset = load( '../datasets/iris_scaled.csv' );
 
+    %% Add sample train & test datasets.
     num_instances = size( dataset, 1 );
 
     train_range = 1 : floor( num_instances * 0.9 );
@@ -16,6 +17,51 @@ function testUnion( testCase )
 
     train_dataset = dataset( train_range, : );
     test_dataset  = dataset( test_range, : );
+
+    testCase.TestData.train_dataset = train_dataset;
+    testCase.TestData.test_dataset  = test_dataset;
+
+    %% Add sample trained mlp model with default parameter.
+    %% But only random_seed is set for test. 
+    mlp = SimpleMLP( 'trainX',      train_dataset( :, 2 : end ), ...
+                     'trainY',      train_dataset( :, 1 ), ...
+                     'testX',       test_dataset( :, 2 : end ), ...
+                     'testY',       test_dataset( :, 1 ), ...
+                     'random_seed', 10000 );
+    testCase.TestData.mlp = mlp.train();
+end
+
+function initialParameterTest( testCase )
+    fprintf( '\nStart initialParameterTest\n' );
+
+    mlp = testCase.TestData.mlp;
+
+    % You can see default parameters in SimpleMLP.m Line 40 - 47
+    % About the way how to set parameters, please see 'testOtherModelTest'
+    verifyEqual( testCase, mlp.learning_rate, 0.01 );
+    verifyEqual( testCase, mlp.count_hneuron, 5 );
+    verifyEqual( testCase, mlp.epochs, 500 );
+    verifyEqual( testCase, mlp.output_activation_fun, 'sig' );
+    verifyEqual( testCase, mlp.activation_fun, 'sig' );
+end
+
+function predictionTest( testCase )
+    fprintf( '\nStart predictionTest\n' );
+
+    mlp = testCase.TestData.mlp;
+    test_dataset = testCase.TestData.test_dataset;
+
+    expected_label        = test_dataset( 1, 1 );
+    [ val actual_output ] = max( mlp.predict( test_dataset( 1, 2 : end ) ) );
+
+    verifyEqual( testCase, expected_label, actual_output );
+end
+
+function testOtherModelTest( testCase )
+    fprintf( '\nStart testOtherModelTest\n' );
+
+    train_dataset = testCase.TestData.train_dataset;
+    test_dataset  = testCase.TestData.test_dataset;
 
     mlp = SimpleMLP( 'trainX', train_dataset( :, 2 : end ), ...
                      'trainY', train_dataset( :, 1 ), ...
@@ -28,67 +74,13 @@ function testUnion( testCase )
                      'output_activation_fun', 'sig', ...
                      'epochs', 300 );
 
-    % You have to call train() yourself
-    % Do not forget to assign trained object.
     trained_mlp = mlp.train();
 
     train_acc = trained_mlp.trainEvaluation();
-    disp( train_acc );
     expected_border = 0.980;
     verifyTrue( testCase, train_acc > expected_border );
 
     test_acc = trained_mlp.testEvaluation();
-    disp( test_acc );
     expected_border = 0.980;
     verifyTrue( testCase, test_acc > expected_border );
-end
-
-function initialParameterTest( testCase )
-    addpath( '../' );
-
-    dataset = load( '../datasets/iris_scaled.csv' );
-
-    num_instances = size( dataset, 1 );
-
-    train_range = 1 : floor( num_instances * 0.9 );
-    test_range  = train_range( end ) + 1 : num_instances;
-
-    train_dataset = dataset( train_range, : );
-    test_dataset  = dataset( test_range, : );
-
-    mlp = SimpleMLP( 'trainX', train_dataset( :, 2 : end ), ...
-                     'trainY', train_dataset( :, 1 ), ...
-                     'testX',  test_dataset( :, 2 : end ), ...
-                     'testY',  test_dataset( :, 1 ));
-
-    % You can see default parameters in SimpleMLP.m Line 40 - 47
-    verifyEqual( testCase, mlp.learning_rate, 0.01 );
-    verifyEqual( testCase, mlp.count_hneuron, 5 );
-    verifyEqual( testCase, mlp.epochs, 500 );
-end
-
-function predictionTest( testCase )
-    addpath( '../' );
-
-    dataset = load( '../datasets/iris_scaled.csv' );
-
-    num_instances = size( dataset, 1 );
-
-    train_range = 1 : floor( num_instances * 0.9 );
-    test_range  = train_range( end ) + 1 : num_instances;
-
-    train_dataset = dataset( train_range, : );
-    test_dataset  = dataset( test_range, : );
-
-    mlp = SimpleMLP( 'trainX', train_dataset( :, 2 : end ), ...
-                     'trainY', train_dataset( :, 1 ), ...
-                     'testX',  test_dataset( :, 2 : end ), ...
-                     'testY',  test_dataset( :, 1 ));
-
-    mlp = mlp.train();
-
-    expected_label        = test_dataset( 1, 1 );
-    [ val actual_output ] = max( mlp.predict( test_dataset( 1, 2 : end ) ) );
-
-    verifyEqual( testCase, expected_label, actual_output );
 end
